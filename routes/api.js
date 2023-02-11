dfebe61d4c672f649df2179aa5ab48d4bfe35159
R3_D4N <bellidoangelo97@gmail.com>
2023-02-11 02:33:02 -0500

    Completed 10 functional tests in 2_functional-tests.js file, and improved request to approve tests

--- routes/api.js
+++ routes/api.js
@@ -1,49 +1,69 @@
 'use strict';
-const {Issue, Project} = require('../models/issue.js')
+const { Issue, Project } = require('../models/issue.js')
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function (req, res) {
       let project = req.params.project;
-      Project.findOne({name: project}).populate('issueIDs').exec((err, projectData)=>{
-        if(err){
+      let {open, assigned_to} = req.query;
+      let query = Project.findOne({ name: project }).populate('issueIDs')
+      if(open){
+        query = Project.findOne({ name: project }).populate({ 
+          path: 'issueIDs',
+          match: {open}
+        })
+      }
+      if(assigned_to){
+        query = Project.findOne({ name: project }).populate({ 
+          path: 'issueIDs',
+          match: {assigned_to}
+        })
+      }
+      if(open && assigned_to){
+        query = Project.findOne({ name: project }).populate({ 
+          path: 'issueIDs',
+          match: {open, assigned_to}
+        })
+      }
+      query.exec((err, projectData) => {
+        if (err) {
           console.log(err);
-        }else{
-          if(!projectData){
+        } else {
+          if (!projectData) {
             res.send('')
-          }else{
+          } else {
             res.send(projectData.issueIDs)
           }
         }
       })
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
       let issueData = req.body
-      Project.findOne({name: project}, (err, dataProject)=>{
-        if(err){
+      Project.findOne({ name: project }, (err, dataProject) => {
+        if (err) {
           console.log(err);
-        }else{
-          Issue.create(issueData, (err, dataIssue)=>{
-            if(err){
-              console.log(err);
-            }else{
-              if(!dataProject){
-                Project.create({name: project, issueIDs: dataIssue._id}, (err,data)=>{
-                  if(err){
+        } else {
+          Issue.create(issueData, (err, dataIssue) => {
+            if (err) {
+              return res.send({ error: 'required field(s) missing' })
+            } else {
+              if (!dataProject) {
+                Project.create({ name: project, issueIDs: dataIssue._id }, (err, data) => {
+                  if (err) {
                     console.log(err);
-                  }else{
+                  } else {
                     return res.send(dataIssue)
                   }
                 })
-              }else{
+              } else {
                 dataProject.issueIDs.push(dataIssue._id)
-                dataProject.save((err)=>{
+                dataProject.save((err) => {
                   if (err) {
                     console.log(err)
-                  }else{
+                  } else {
                     return res.send(dataIssue)
                   }
                 })
@@ -53,43 +73,45 @@ module.exports = function (app) {
         }
       })
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
       let data = req.body
-      Project.findOne({name: project}, (err, dataProject)=>{
-        if(err){
+      if (!data._id) return res.send({error: 'missing _id'})
+      if (Object.keys(data).length <2) return res.send({ error: 'no update field(s) sent', '_id': data._id })
+      Project.findOne({ name: project }, (err, dataProject) => {
+        if (err) {
           console.log(err);
-        }else{
+        } else {
           if (!dataProject) {
-            res.send({msj: 'project name not found'})
-          }else{
-            Issue.findOneAndUpdate({_id: data._id}, {open: data.open}, {new: true}, (err, issueData)=>{
-              if(err){
-                console.log(err);
-              }else{
-                res.send(issueData)
+            res.send({ msj: 'project name not found' })
+          } else {
+            Issue.findOneAndUpdate({ _id: data._id }, { open: data.open }, (err, issueData) => {
+              if (err) {
+                return res.send({ error: 'could not update', '_id': data._id })
+              } else {
+                res.send({result: 'successfully updated', _id: issueData._id})
               }
             })
           }
         }
       })
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
       let data = req.body
-      Project.findOne({name: project}, (err, dataProject)=>{
-        if(err){
+      Project.findOne({ name: project }, (err, dataProject) => {
+        if (err) {
           console.log(err);
-        }else{
+        } else {
           if (!dataProject) {
-            res.send({msj: 'project name not found'})
-          }else{
-            Issue.deleteOne({_id: data._id}, (err, issueData)=>{
-              if(err){
+            res.send({ msj: 'project name not found' })
+          } else {
+            Issue.deleteOne({ _id: data._id }, (err, issueData) => {
+              if (err) {
                 console.log(err);
-              }else{
+              } else {
                 res.send(issueData)
               }
             })
@@ -97,5 +119,5 @@ module.exports = function (app) {
         }
       })
     });
-    
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,147 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  // #1
+  test('Post required and optionally fields', function(done){
+    chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+            'issue_title': 'fourth',
+            'issue_text': 'post test 1',
+            'created_by': 'angelo',
+            'assigned_to': 'sandra',
+            'status_text': 'test'
+        })
+        .end(function(err, res){
+            assert.isNotNull(res.text , 'Post with all data')
+            done()
+        })
+  })
+  // #2
+  test('Post required fields', function(done){
+    chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+            'issue_title': 'fifth',
+            'issue_text': 'post test 2',
+            'created_by': 'angelo'
+        })
+        .end(function(err, res){
+            assert.isNotNull(res.text , 'Post with all data')
+            done()
+        })
+  })
+  // #3
+  test('Post with missing required fields', function(done){
+    chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+            'issue_title': 'fifth',
+            'issue_text': 'post test 2'
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"error":"required field(s) missing"}', 'Post with all data')
+            done()
+        })
+  })
+  // #4
+  test('Get request for an array of all issues', function(done){
+    chai
+        .request(server)
+        .get('/api/issues/test')
+        .end(function(err, res){
+            assert.isNotNull(res.text, 'Get all issues data')
+            done()
+        })
+  })
+  // #5
+  test('Get request with one filter', function(done){
+    chai
+        .request(server)
+        .get('/api/issues/test?open=false')
+        .end(function(err, res){
+            assert.isNotNull(res.text, 'Get issues with one filter')
+            done()
+        })
+  })
+  // #6
+  test('Get request with one filter', function(done){
+    chai
+        .request(server)
+        .get('/api/issues/test?open=true&assigned_to=camila')
+        .end(function(err, res){
+            assert.isNotNull(res.text, 'Get issues with multiple filters')
+            done()
+        })
+  })
+  // #7
+  test('Put request update one field', function(done){
+    chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+            '_id': '63e5eae0901336f1631a512f',
+            'open': true
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"result":"successfully updated","_id":"63e5eae0901336f1631a512f"}')
+            done()
+        })
+  })/*
+  // #8 todo
+  test('Put request update multiple fields', function(done){
+    chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+            '_id': '63e5eae0901336f1631a512f',
+            'open': true
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"result":"successfully updated", "_id":"63e5eae0901336f1631a512f"}', 'Put with all data')
+        })
+  })*/
+  // #9
+  test('Put request with missing _id', function(done){
+    chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+            'open': true
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"error":"missing _id"}', 'Put without _id')
+            done()
+        })
+  })
+  // #10
+  test('Put request with no fields to update', function(done){
+    chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+            '_id': '63e5eae0901336f1631a512f'
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"error":"no update field(s) sent","_id":"63e5eae0901336f1631a512f"}', 'Put without field to update')
+            done()
+        })
+  })
+  // #11
+  test('Put request with a wrong _id', function(done){
+    chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+            '_id': '63e5eaeff01336f1631a512f',
+            'open': true
+        })
+        .end(function(err, res){
+            assert.strictEqual(res.text, '{"error":"could not update","_id":"63e5eaeff01336f1631a512f"}', 'Put with wrong _id')
+            done()
+        })
+  })
 });
